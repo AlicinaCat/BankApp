b6eeabbc175e8582e29cc0c0dfcd7fb52afc2c75
alicinacat <alice.casaburi@gmail.com>
2019-05-14 15:29:07 +0200

    first commit

---
+++ BankApp.App/App.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BankApp.Domain;
+
+namespace BankApp.Domain
+{
+    public class Bank
+    {
+        private IUserDataStore userDataStore;
+
+        public Bank(IUserDataStore userDataStore)
+        {
+            this.userDataStore = userDataStore;
+        }
+
+        public Account GetAccount(string accountId)
+        {
+            return userDataStore.GetAccount(accountId);
+        }
+
+        public void Deposit(string accountId, decimal amount)
+        {
+            //GetAccount(accountId).Deposit(amount);
+        }
+
+        public void Withdraw(string accountId, decimal amount)
+        {
+            //GetAccount(accountId).Withdraw(amount);
+        }
+
+        public void Transfer(string accountFromId, string accountToId, decimal amount)
+        {
+            //GetAccount(accountFromId).Withdraw(amount);
+            //GetAccount(accountToId).Deposit(amount);
+        }
+    }
+}
---
+++ BankApp.Data/IUserDataStore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using BankApp.Domain;
+
+namespace BankApp.Domain
+{
+    public interface IUserDataStore
+    {
+
+    }
+}
