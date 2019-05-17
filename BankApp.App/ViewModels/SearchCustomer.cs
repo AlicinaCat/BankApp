51769762b2659a5fefba1b45bf29f54b3d61cb02
alicinacat <alice.casaburi@gmail.com>
2019-05-17 23:35:17 +0200

    Added search functionality and view component.

---
+++ BankApp.App/ViewModels/SearchCustomer.cs
@@ -0,0 +1,29 @@
+﻿using BankApp.App.Accounts.Queries;
+using BankApp.Data;
+using BankApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp.App.ViewModels
+{
+    public class SearchCustomer
+    {
+        private readonly BankAppDataContext context;
+        private Customer_Queries customer_queries;
+
+        public string UserInput { get; set; }
+        public List<Customer> SearchResults { get; set; }
+
+        public SearchCustomer()
+        {
+            this.context = new BankAppDataContext();
+            this.customer_queries = new Customer_Queries(context);
+        }
+
+        public List<Customer> GetResults()
+        {
+            return SearchResults = customer_queries.SearchCustomers(UserInput);
+        }
+    }
+}
