d6dab9549ed5a3002bafca189ac7cb8f4af49930
alicinacat <alice.casaburi@gmail.com>
2019-05-17 10:28:56 +0200

    Added customer queries

---
+++ BankApp.App/Customers/Queries/Customer_Queries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BankApp.Data;
+using BankApp.Domain;
+
+namespace BankApp.App.Accounts.Queries
+{
+    public class Customer_Queries
+    {
+        private readonly BankAppDataContext context;
+
+        public Customer_Queries(BankAppDataContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return context.Customers.ToList();
+        }
+    }
+}
