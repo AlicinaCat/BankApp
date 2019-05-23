9a91eeab71572fd154ded1700a8abd055163c2e9
alicinacat <alice.casaburi@gmail.com>
2019-05-23 13:54:47 +0200

    Added disposition command handler

---
+++ BankApp.App/Dispositions/Commands/DispositionCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using BankApp.App.Accounts.Commands;
+using BankApp.Data;
+using BankApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp.App.Dispositions.Commands
+{
+    public class DispositionCommandHandler
+    {
+        private BankAppDataContext context;
+        AccountCommandHandler accountCommandHandler;
+
+        public DispositionCommandHandler(BankAppDataContext context)
+        {
+            this.context = context;
+            accountCommandHandler = new AccountCommandHandler(context);
+        }
+
+        public void CreateNewDisposition(int customerId)
+        {
+            Disposition disposition = new Disposition()
+            {
+                CustomerId = customerId,
+                AccountId = accountCommandHandler.CreateNewAccount(),
+                Type = "OWNER"
+            };
+
+            context.Add(disposition);
+            context.SaveChanges();
+        }
+    }
+}
