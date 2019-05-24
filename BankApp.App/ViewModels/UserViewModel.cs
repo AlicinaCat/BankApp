b1a36d3d31ad1e183d5a6e48d0a75a03de8fdade
alicinacat <alice.casaburi@gmail.com>
2019-05-24 11:51:57 +0200

    Added functionality for register, login, logout

---
+++ BankApp.App/ViewModels/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BankApp.App.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
